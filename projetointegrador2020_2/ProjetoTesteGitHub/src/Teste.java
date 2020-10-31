7797fe17b57c6a6ca9914a7310aacff51fadd58a
Andreis-Silva <andreisms02@gmail.com>
2020-10-30 22:31:53 -0300

    Modificada variavel de soma.

--- projetointegrador2020_2/ProjetoTesteGitHub/src/Teste.java
+++ projetointegrador2020_2/ProjetoTesteGitHub/src/Teste.java
@@ -4,7 +4,7 @@ public class Teste {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 		int a = 100;
-		int soma = a + 100;
+		int somaTotal = a + 100;
 		// teste
 		// outro
 		// nova linha
