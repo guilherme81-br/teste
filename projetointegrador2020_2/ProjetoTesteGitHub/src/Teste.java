75cbc4e450d4b6c3654741f06d19d05d6c6e9e11
str8ping <vg.p@me.com>
2020-10-30 22:36:20 -0300

    Alterei a variavel a

--- projetointegrador2020_2/ProjetoTesteGitHub/src/Teste.java
+++ projetointegrador2020_2/ProjetoTesteGitHub/src/Teste.java
@@ -3,7 +3,7 @@ public class Teste {
 	
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
-		int a = 100;
+		int a = 10000;
 		int somaTotal = a + 100;
 		int mult = somaTotal * 2;
 		// teste
